81f03fe2362b4fa4e73a82dcd36e23f67b51152a
Phan Kang Min <phankangmin@MacBook-Air-cua-Phan.local>
2022-08-04 23:31:04 +0700

    update date

--- src/data/songList.js
+++ src/data/songList.js
@@ -8,7 +8,42 @@ import conmuahoangda from "../assets/audio/ALBUM_TAM HON CUA DA/ROCK XUYEN MAN D
 import nguoidanbahoada from "../assets/audio/ALBUM_TAM HON CUA DA/NGUOI DAN BA HOA DA.mp3";
 import niemtinchocatbui from "../assets/audio/ALBUM_TAM HON CUA DA/NIEM TIN CHO CAT BUI.mp3";
 import duongdenngayvinhquang from "../assets/audio/ALBUM_TAM HON CUA DA/DUONG DEN NGAY VINH QUANG.mp3";
+import baicasonghong from "../assets/audio/ALBUM_VO HINH/BAI CA SONG HONG.mp3";
+import caybang from "../assets/audio/ALBUM_VO HINH/CAY BANG.mp3";
+import conso0 from "../assets/audio/ALBUM_VO HINH/CON SO 0.mp3";
+import khampha from "../assets/audio/ALBUM_VO HINH/KHAM PHA.mp3";
+import matden from "../assets/audio/ALBUM_VO HINH/MAT DEN.mp3";
 import chavacon from "../assets/audio/ALBUM_NAM CHAM/Cha-Va-Con-Buc-Tuong.mp3";
+import chuyentinhcuathuythan from "../assets/audio/ALBUM_NAM CHAM/Chuyen-Tinh-Cua-Thuy-Than-Buc-Tuong.mp3";
+import dauvetnghietnga from "../assets/audio/ALBUM_NAM CHAM/Dau-Vet-Nghiet-Nga-Buc-Tuong.mp3";
+import demen from "../assets/audio/ALBUM_NAM CHAM/De-Men-Buc-Tuong.mp3";
+import doibantay from "../assets/audio/ALBUM_NAM CHAM/Doi-Ban-Tay-Buc-Tuong.mp3";
+import namcham from "../assets/audio/ALBUM_NAM CHAM/Nam-Cham-Buc-Tuong.mp3";
+import neuemhieu from "../assets/audio/ALBUM_NAM CHAM/Neu-Em-Hieu-Buc-Tuong.mp3";
+import nguoimongdu from "../assets/audio/ALBUM_NAM CHAM/Nguoi-Mong-Du-Buc-Tuong.mp3";
+import rakhoi from "../assets/audio/ALBUM_NAM CHAM/Ra-Khoi-Buc-Tuong.mp3";
+import trove from "../assets/audio/ALBUM_NAM CHAM/Tro-Ve-Buc-Tuong.mp3";
+import anhseden from "../assets/audio/ALBUM_NGAY KHAC/Anh-Se-Den-Buc-Tuong.mp3";
+import chobuocchanemdi from "../assets/audio/ALBUM_NGAY KHAC/Cho-Buoc-Chan-Em-Di-Buc-Tuong.mp3";
+import choitrau from "../assets/audio/ALBUM_NGAY KHAC/Choi-Trau-Buc-Tuong.mp3";
+import choivoi from "../assets/audio/ALBUM_NGAY KHAC/Choi-Voi-Buc-Tuong.mp3";
+import cogaiquangkhando from "../assets/audio/ALBUM_NGAY KHAC/Co-Gai-Quang-Khan-Do-Buc-Tuong.mp3";
+import hoabantrang from "../assets/audio/ALBUM_NGAY KHAC/Hoa-Ban-Trang-Buc-Tuong.mp3";
+import moi from "../assets/audio/ALBUM_NGAY KHAC/Moi-Buc-Tuong.mp3";
+import ngaykhac from "../assets/audio/ALBUM_NGAY KHAC/Ngay-Khac-Buc-Tuong.mp3";
+import nguoibao from "../assets/audio/ALBUM_NGAY KHAC/Nguoi-Bao-Buc-Tuong.mp3";
+import rungchuongvang from "../assets/audio/ALBUM_NGAY KHAC/Rung-Chuong-Vang-Buc-Tuong.mp3";
+import tienggoi from "../assets/audio/ALBUM_NGAY KHAC/Tieng-Goi-Buc-Tuong.mp3";
+import conmuathang5 from "../assets/audio/ALBUM_DAT VIET/Con-Mua-Thang-5-Buc-Tuong.mp3";
+import datviet from "../assets/audio/ALBUM_DAT VIET/Dat-Viet-Buc-Tuong.mp3";
+import dauan from "../assets/audio/ALBUM_DAT VIET/Dau-An-Buc-Tuong.mp3";
+import hiem from "../assets/audio/ALBUM_DAT VIET/Hiem-Buc-Tuong.mp3";
+import mensay from "../assets/audio/ALBUM_DAT VIET/Men-Say-Buc-Tuong.mp3";
+import nhungchuyendidai from "../assets/audio/ALBUM_DAT VIET/Nhung-Chuyen-Di-Dai-Buc-Tuong.mp3";
+import song from "../assets/audio/ALBUM_DAT VIET/Song-Buc-Tuong.mp3";
+import thang12 from "../assets/audio/ALBUM_DAT VIET/Thang-12-Buc-Tuong.mp3";
+import tuoi20 from "../assets/audio/ALBUM_DAT VIET/Tuoi-20-Buc-Tuong.mp3";
+import uocmocuatoi from "../assets/audio/ALBUM_DAT VIET/Uoc-Mo-Cua-Toi-Buc-Tuong.mp3";
 
 const SONG_LIST = [
     {
@@ -103,30 +138,30 @@ const SONG_LIST = [
     },
     {
         id: "s000011",
-        nameEn: "The song of Hong river",
+        nameEn: "Song of the Red River",
         nameVi: "Bài ca sông Hồng",
         album: "a000002",
         img: "Vô hình",
-        src: bonghongthuytinh,
-        time: ""
+        src: baicasonghong,
+        time: "6:06"
     },
     {
         id: "s000012",
-        nameEn: "School Trees",
+        nameEn: "Eagletree",
         nameVi: "Cây bàng",
         album: "a000002",
         img: "Vô hình",
-        src: bonghongthuytinh,
-        time: ""
+        src: caybang,
+        time: "7:39"
     },
     {
         id: "s000013",
-        nameEn: "Zero",
+        nameEn: "Number 0",
         nameVi: "Con so 0",
         album: "a000002",
         img: "Vô hình",
-        src: bonghongthuytinh,
-        time: ""
+        src: conso0,
+        time: "5:30"
     },
     {
         id: "s000014",
@@ -134,8 +169,8 @@ const SONG_LIST = [
         nameVi: "Khám phá",
         album: "a000002",
         img: "Vô hình",
-        src: bonghongthuytinh,
-        time: ""
+        src: khampha,
+        time: "4:29"
     },
     {
         id: "s000015",
@@ -143,8 +178,8 @@ const SONG_LIST = [
         nameVi: "Mắt đen",
         album: "a000002",
         img: "Vô hình",
-        src: bonghongthuytinh,
-        time: ""
+        src: matden,
+        time: "4:07"
     },
     {
         id: "s000016",
@@ -153,7 +188,277 @@ const SONG_LIST = [
         album: "a000003",
         img: "Nam Châm",
         src: chavacon,
-        time: ""
+        time: "3:53"
+    },
+    {
+        id: "s000017",
+        nameEn: "Love story of water god",
+        nameVi: "Chuyện tình của thuỷ thần",
+        album: "a000003",
+        img: "Nam Châm",
+        src: chuyentinhcuathuythan,
+        time: "6:17"
+    },
+    {
+        id: "s000018",
+        nameEn: "Grim traces",
+        nameVi: "Dấu vết nghiệt ngã",
+        album: "a000003",
+        img: "Nam Châm",
+        src: dauvetnghietnga,
+        time: "7:02"
+    },
+    {
+        id: "s000019",
+        nameEn: "Cricket",
+        nameVi: "Dế mèn",
+        album: "a000003",
+        img: "Nam Châm",
+        src: demen,
+        time: "6:57"
+    },
+    {
+        id: "s000020",
+        nameEn: "Hands",
+        nameVi: "Đôi bàn tay",
+        album: "a000003",
+        img: "Nam Châm",
+        src: doibantay,
+        time: "5:10"
+    },
+    {
+        id: "s000021",
+        nameEn: "Magnet",
+        nameVi: "Nam châm",
+        album: "a000003",
+        img: "Nam Châm",
+        src: namcham,
+        time: "4:36"
+    },
+    {
+        id: "s000022",
+        nameEn: "If you understand",
+        nameVi: "Nếu em hiểu",
+        album: "a000003",
+        img: "Nam Châm",
+        src: neuemhieu,
+        time: "4:47"
+    },
+    {
+        id: "s000023",
+        nameEn: "Sleepwalker",
+        nameVi: "Người mộng du",
+        album: "a000003",
+        img: "Nam Châm",
+        src: nguoimongdu,
+        time: "4:34"
+    },
+    {
+        id: "s000024",
+        nameEn: "Rowing",
+        nameVi: "Ra khơi",
+        album: "a000003",
+        img: "Nam Châm",
+        src: rakhoi,
+        time: "4:38"
+    },
+    {
+        id: "s000025",
+        nameEn: "Comeback",
+        nameVi: "Trở về",
+        album: "a000003",
+        img: "Nam Châm",
+        src: trove,
+        time: "3:23"
+    },
+    {
+        id: "s000026",
+        nameEn: "I will come",
+        nameVi: "Anh sẽ đến",
+        album: "a000004",
+        img: "Ngày Khác",
+        src: anhseden,
+        time: "4:24"
+    },
+    {
+        id: "s000027",
+        nameEn: "Let me walk",
+        nameVi: "Chờ bước chân em đi",
+        album: "a000004",
+        img: "Ngày Khác",
+        src: chobuocchanemdi,
+        time: "4:28"
+    },
+    {
+        id: "s000028",
+        nameEn: "Buffalo fighting",
+        nameVi: "Chọi trâu",
+        album: "a000004",
+        img: "Ngày Khác",
+        src: choitrau,
+        time: "3:42"
+    },
+    {
+        id: "s000029",
+        nameEn: "Overbalance",
+        nameVi: "Chơi vơi",
+        album: "a000004",
+        img: "Ngày Khác",
+        src: choivoi,
+        time: "5:15"
+    },
+    {
+        id: "s000030",
+        nameEn: "Red Riding Hood girl",
+        nameVi: "Cô gái quàng khăn đỏ",
+        album: "a000004",
+        img: "Ngày Khác",
+        src: cogaiquangkhando,
+        time: "4:24"
+    },
+    {
+        id: "s000031",
+        nameEn: "Flower White board",
+        nameVi: "Hoa ban trắng",
+        album: "a000004",
+        img: "Ngày Khác",
+        src: hoabantrang,
+        time: "7:02"
+    },
+    {
+        id: "s000032",
+        nameEn: "Muscle fatigue",
+        nameVi: "Mỏi",
+        album: "a000004",
+        img: "Ngày Khác",
+        src: moi,
+        time: "4:10"
+    },
+    {
+        id: "s000033",
+        nameEn: "Another date",
+        nameVi: "Ngày khác",
+        album: "a000004",
+        img: "Ngày Khác",
+        src: ngaykhac,
+        time: "5:06"
+    },
+    {
+        id: "s000034",
+        nameEn: "Storm man",
+        nameVi: "Người bão",
+        album: "a000004",
+        img: "Ngày Khác",
+        src: nguoibao,
+        time: "4:02"
+    },
+    {
+        id: "s000035",
+        nameEn: "Ring the golden bell",
+        nameVi: "Rung chuông vàng",
+        album: "a000004",
+        img: "Ngày Khác",
+        src: rungchuongvang,
+        time: "3:52"
+    },
+    {
+        id: "s000036",
+        nameEn: "The sound",
+        nameVi: "Tiếng gọi",
+        album: "a000004",
+        img: "Ngày Khác",
+        src: tienggoi,
+        time: "4:05"
+    },
+    {
+        id: "s000037",
+        nameEn: "May rain",
+        nameVi: "Cơn mưa tháng 5",
+        album: "a000005",
+        img: "Đất Việt",
+        src: conmuathang5,
+        time: "4:03"
+    },
+    {
+        id: "s000038",
+        nameEn: "Vietnamese land",
+        nameVi: "Đất Việt",
+        album: "a000005",
+        img: "Đất Việt",
+        src: datviet,
+        time: "8:52"
+    },
+    {
+        id: "s000039",
+        nameEn: "Imprint",
+        nameVi: "Dấn ấn",
+        album: "a000005",
+        img: "Đất Việt",
+        src: dauan,
+        time: "5:01"
+    },
+    {
+        id: "s000040",
+        nameEn: "Rare",
+        nameVi: "Hiếm",
+        album: "a000005",
+        img: "Đất Việt",
+        src: hiem,
+        time: "4:05"
+    },
+    {
+        id: "s000041",
+        nameEn: "Drunken yeast",
+        nameVi: "Men say",
+        album: "a000005",
+        img: "Đất Việt",
+        src: mensay,
+        time: "6:48"
+    },
+    {
+        id: "s000042",
+        nameEn: "Long trips",
+        nameVi: "Những chuyến đi dài",
+        album: "a000005",
+        img: "Đất Việt",
+        src: nhungchuyendidai,
+        time: "3:46"
+    },
+    {
+        id: "s000043",
+        nameEn: "Wave",
+        nameVi: "Sóng",
+        album: "a000005",
+        img: "Đất Việt",
+        src: song,
+        time: "3:16"
+    },
+    {
+        id: "s000044",
+        nameEn: "December",
+        nameVi: "Tháng 12",
+        album: "a000005",
+        img: "Đất Việt",
+        src: thang12,
+        time: "4:35"
+    },
+    {
+        id: "s000045",
+        nameEn: "20 years old",
+        nameVi: "Tuổi 20",
+        album: "a000005",
+        img: "Đất Việt",
+        src: tuoi20,
+        time: "3:24"
+    },
+    {
+        id: "s000046",
+        nameEn: "My dream",
+        nameVi: "Ước mơ của tôi",
+        album: "a000005",
+        img: "Đất Việt",
+        src: uocmocuatoi,
+        time: "3:37"
     },
 ]
 
